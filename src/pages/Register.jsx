b4a02e559c1bb933798dea0508f795b2e5d0a907
Petar Markota <pmarko1@racunarstvo.hr>
2023-10-21 21:03:31 +0200

    1.02

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -96,7 +96,7 @@ export const Register = () => {
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Already have an account?{" "}
                   <Link
-                    to="/register"
+                    to="/login"
                     className="font-medium text-blue-600 hover:underline dark:text-blue-500"
                   >
                     Log in
